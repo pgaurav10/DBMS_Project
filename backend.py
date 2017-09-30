2e1581910a655ddd1ccfb9a4b49d5d12407f131f
Gaurav24121997 <gauravpatil24121997@gmail.com>
2017-09-30 15:16:50 +0530

    Create backend.py

---
+++ backend.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 11:38:08 2017
+
+@author: Gaurav
+"""
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("bk.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book_p (id INTEGER PRIMARY KEY, title text, author text, quantity text, price text)")
+    conn.commit()
+    conn.close()
+
+def insert(title,author,quantity,price):
+    conn=sqlite3.connect("bk.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book_p VALUES (NULL,?,?,?,?)",(title,author,quantity,price))
+    conn.commit()
+    conn.close()
+    view()
+
+def view():
+    conn=sqlite3.connect("bk.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book_p")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="",author="",quantity="",price=""):
+    conn=sqlite3.connect("bk.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book_p WHERE title=? OR author=? OR quantity=? OR price=?", (title,author,quantity,price))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("bk.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book_p WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(id,title,author,quantity,price):
+    conn=sqlite3.connect("bk.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book_p SET title=?, author=?, quantity=?, price=? WHERE id=?",(title,author,quantity,price,id))
+    conn.commit()
+    conn.close()
+
+connect()
+
+
